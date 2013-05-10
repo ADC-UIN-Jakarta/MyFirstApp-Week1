42b7d76604b9547d0ef7453d0426bd93a3bd2258
Luthfi Hariz <lavalyst@gmail.com>
2013-05-10 17:46:46 +0700

    initial commit

---
+++ src/com/example/myfirstapp/HelloWorldActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HelloWorldActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	
+}
